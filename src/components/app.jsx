ecaf2224936292aa774a3e50dcbe6da1a1d85a3d
Gino Carrasco <gcarrasco@belatrix.com>
2017-10-02 08:33:22 -0500

    Removed react-addons-perf

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Perf from 'react-addons-perf';
-
-if (typeof window !== 'undefined') window.Perf = Perf;
 
 export default class App extends React.Component {
     static propTypes = {
